5d1f6885aed18de88e6af2919288b87a4b879695
Maddie Rogers <madisonrogers@mymail.mines.edu>
2017-12-08 11:52:11 -0700

    got the database creation to magically work

--- sql/database.sql
+++ sql/database.sql
@@ -1,20 +1,23 @@
-DROP TABLE IF EXISTS person CASCADE;
-CREATE TABLE person {
+SET FOREIGN_KEY_CHECKS=0;
+DROP TABLE IF EXISTS person;
+CREATE TABLE person 
+(
 	person_id INT AUTO_INCREMENT,
-	first_name VARCHAR(255),
-	last_name VARCHAR(255),
-	username VARCHAR(255),
-	password VARCHAR(255),
+	full_name VARCHAR(255) NOT NULL,
+	email VARCHAR(255) NOT NULL,
+	username VARCHAR(255) NOT NULL,
+	password VARCHAR(255) NOT NULL,
 	PRIMARY KEY (person_id)
-};
+);
 
 DROP TABLE IF EXISTS article CASCADE;
-CREATE TABLE article {
+CREATE TABLE article
+(
 	article_id INT AUTO_INCREMENT,
-	fk_person_id VARCHAR(255),
-	title VARCHAR(255),
-	article_text TEXT,
+	fk_person_id INT NOT NULL,
+	title VARCHAR(255) NOT NULL,
+	article_text TEXT NOT NULL,
 	time_created VARCHAR(255),
 	PRIMARY KEY (article_id),
-	FOREIGN KEY (fk_person_id) REFERENCES person(person_id)
-};
+	FOREIGN KEY (fk_person_id) REFERENCES person(person_id) ON DELETE CASCADE
+);
